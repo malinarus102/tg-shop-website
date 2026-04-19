a796aed30a55a4eba4f777c51bb8b27f5ebe8742
malinarus102 <malinarus102@gmail.com>
2026-04-19 11:36:41 +0300

    fixed

--- bot_runner.py
+++ bot_runner.py
@@ -1,3 +1,10 @@
+import requests
+import os
+
+token = os.getenv("BOT_TOKEN", "")
+if token:
+    requests.get(f"https://api.telegram.org/bot{token}/deleteWebhook?drop_pending_updates=true")
+
 from src.bot import main
 
 if __name__ == '__main__':
